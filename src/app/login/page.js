e1a77907e5d3593eadd498b6110ec00476abdd9c
ParkGiraffe <op5321@soongsil.ac.kr>
2024-10-09 15:35:10 +0900

    Feat: 로그인 성공하면 홈 화면으로 가게 구현

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation"; // useRouter 훅 사용
 import { auth, db } from "../../../lib/firebase";
 import Modal from "@/app/_component/login/Modal";
 import SignupForm from "@/app/_component/login/SignupForm";
@@ -12,6 +13,8 @@ export default function Login() {
   const [error, setError] = useState(null);
   const [isSignupOpen, setIsSignupOpen] = useState(false);
 
+  const router = useRouter(); // useRouter 훅 초기화
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(null);
@@ -19,6 +22,7 @@ export default function Login() {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       // 로그인 성공 후 원하는 페이지로 리다이렉트
+      router.push("/"); // 메인 페이지로 이동
     } catch (err) {
       setError(err.message);
     }
@@ -56,7 +60,7 @@ export default function Login() {
 
       {/* 회원가입 모달 */}
       <Modal isOpen={isSignupOpen} onClose={closeSignupModal}>
-        <SignupForm auth={auth} db={db} onSuccess={closeSignupModal}/>
+        <SignupForm auth={auth} db={db} onSuccess={closeSignupModal} />
       </Modal>
 
       <style jsx>{`
