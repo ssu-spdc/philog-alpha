3c35fa27e4d9e667e4707289b850345c8e46a770
ParkGiraffe <op5321@soongsil.ac.kr>
2024-11-05 20:47:54 +0900

    Feat: 문의사항 보낼 때 팝업창, 에러메시지 출력

--- src/app/question/page.jsx
+++ src/app/question/page.jsx
@@ -16,31 +16,40 @@ export default function QuestionPage() {
   const [question, setQuestion] = useState("");
   const [email, setEmail] = useState("");
   const [isUploading, setIsUploading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
 
   const isReady = question && email;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsUploading(true);
-    // setError(null);
+    setError(null);
 
-    // 필드가 비어있는지 확인
-    if (!email || !question) {
-      // setError("이메일과 문의 내용을 모두 입력해주세요.");
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      setError("올바른 이메일 형식을 입력해주세요.");
+      setIsUploading(false);
       return;
     }
 
+    if (!question) {
+      setError("문의 내용을 입력해주세요.");
+      setIsUploading(false);
+      return;
+    }
     try {
       await setDoc(doc(db, "question", email), {
         email: email,
         question: question,
       });
-
-      // router.push("/");
-      setIsUploading(false);
     } catch (err) {
       console.log(err);
+      setError("문의 등록 중 문제가 발생했습니다.");
+    } finally {
+      setIsUploading(false);
+      setShowModal(true);
     }
   };
 
@@ -51,10 +60,9 @@ export default function QuestionPage() {
           style={{ flexDirection: "column", gap: "20px", marginTop: "20px" }}
         >
           <SectionTitle style={{ marginBottom: "4px" }}>문의하기</SectionTitle>
-
           <EmailInput email={email} setEmail={setEmail} />
-
           <QuestionInput question={question} setQuestion={setQuestion} />
+          {error && <ErrorText>{error}</ErrorText>}
           <WriteBtn
             onClick={handleSubmit}
             disabled={!isReady || isUploading}
@@ -63,6 +71,19 @@ export default function QuestionPage() {
           >
             {isUploading ? "업로드 중" : "등록하기"}
           </WriteBtn>
+          {showModal && (
+            <ConfirmModal>
+              <ModalContent>
+                <p>문의사항이 등록되었습니다.</p>
+                <ModalActions>
+                  <div />
+                  <ConfirmButton onClick={() => setShowModal(false)}>
+                    확인
+                  </ConfirmButton>
+                </ModalActions>
+              </ModalContent>
+            </ConfirmModal>
+          )}
         </PageContainer>
       </MobileDisplay>
     </Main>
@@ -85,4 +106,48 @@ const WriteBtn = styled.button`
     color: #a6abaf;
     cursor: not-allowed;
   }
+`;
+
+const ErrorText = styled.p`
+  color: red;
+  font-size: 14px;
+  margin-top: -10px;
+`;
+
+// 모달 스타일
+const ConfirmModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 30px;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  font-weight: 500;
+`;
+
+const ModalActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+const ConfirmButton = styled.button`
+  background-color: #e0e5ea;
+  color: #a6abaf;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
 `;
